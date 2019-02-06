cd65e77b99056915ced121f9b14896d248544680
AJ <jardineang9@gmail.com>
2019-02-05 20:50:42 -0500

    AddedCorrectDbContext[#163674514]

---
+++ AquaLearn/AquaLearn.Data/AquaLearnDbContext.cs
@@ -0,0 +1,36 @@
+﻿using AquaLearn.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaLearn.Data
+{
+    public class AquaLearnDbContext : DbContext
+    {
+        public DbSet<Fish> Fish { get; set; }
+        public DbSet<Plant> Plant { get; set; }
+        public DbSet<Trash> Trash { get; set; }
+        public DbSet<Hazard> Hazard { get; set; }
+        public DbSet<Exhibit> Exhibit { get; set; }
+        public DbSet<WaterType> WaterType { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("server=ajdotnet2019.database.windows.net;database=AquaLearnDB;user id=sqladmin;password=Florida2019;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Fish>().HasKey(e => e.FishId);
+            builder.Entity<WaterType>().HasKey(e => e.WaterTypeId);
+            builder.Entity<Plant>().HasKey(e => e.PlantId);
+            builder.Entity<Trash>().HasKey(e => e.TrashId);
+            builder.Entity<Exhibit>().HasKey(e => e.ExhibitId);
+            builder.Entity<Fish>().HasKey(e => e.HazardId);
+
+        }
+
+    }
+}
--- AquaLearn/AquaLearn.Data/Helpers/AquaLearnDBContext.cs
+++ AquaLearn/AquaLearn.Data/Helpers/AquaLearnDBContext.cs
@@ -7,124 +7,118 @@ using System.Text;
 namespace AquaLearn.Data.Helpers
 {
     public class AquaLearnDBContext
-    { 
-        public AquaLearnDBContext()
     {
+        public AquaLearnDBContext()
+        {
+            
+        }
 
-    }
+        public AquaLearnDBContext(DbContextOptions<AquaLearnDBContext> options)
+            : base(options)
+        {
+        }
 
-            public AquaLearnDBContext(DbContextOptions<AquaLearnDBContext> options)
-                : base(options)
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
             {
-            }
-
-            public virtual DbSet<Fish> Fish { get; set; }
-            public virtual DbSet<Plant> Plant { get; set; }
-            public virtual DbSet<Trash> Trash { get; set; }
-            public virtual DbSet<Hazard> Hazard { get; set; }
-            public virtual DbSet<Exhibit> Exhibit { get; set; }
-            public virtual DbSet<WaterType> WaterType { get; set; }
-    
-
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                if (!optionsBuilder.IsConfigured)
-                {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                    optionsBuilder.UseSqlServer("server=ajdotnet2019.database.windows.net;database=MovieNightDB;user id=sqladmin;password=Florida2019;");
-                }
+                optionsBuilder.UseSqlServer("server=ajdotnet2019.database.windows.net;database=MovieNightDB;user id=sqladmin;password=Florida2019;");
             }
+        }
 
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<WaterType>(entity =>
             {
-                modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+                entity.ToTable("WaterType", "Environment");
 
-                modelBuilder.Entity<WaterType>(entity =>
-                {
-                    entity.ToTable("WaterType", "Environment");
-
-                    entity.Property(e => e.Name)
-                        .IsRequired()
-                        .HasMaxLength(100);
-
-                });
-
-                  modelBuilder.Entity<Exhibit>(entity =>
-                {
-                      entity.ToTable("Exhibit", "Environment");
-
-                     entity.Property(e => e.Name)
+                entity.Property(e => e.Name)
                     .IsRequired()
                     .HasMaxLength(100);
 
-                    entity.HasOne(d => d.WaterType)
-                    .WithMany(WaterType => Exhibit)
+            });
+
+            modelBuilder.Entity<Exhibit>(entity =>
+          {
+              entity.ToTable("Exhibit", "Environment");
+
+              entity.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+              entity.HasOne(d => d.WaterType)
+              .WithMany(WaterType => Exhibit)
+              .HasForeignKey(d => d.WaterTypeId)
+              .HasConstraintName("FK_Exhibit_WaterTypeId");
+          });
+
+            modelBuilder.Entity<Hazard>(entity =>
+                    {
+                        entity.ToTable("Hazards", "HumanEffects");
+
+                        entity.Property(e => e.Name)
+                            .IsRequired()
+                            .HasMaxLength(100);
+
+                        entity.Property(e => e.Description)
+                            .IsRequired()
+                            .HasMaxLength(500);
+                    });
+
+            modelBuilder.Entity<Trash>(entity =>
+            {
+                entity.Property(e => e.Name)
+                         .IsRequired()
+                         .HasMaxLength(100);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(500);
+            });
+
+            modelBuilder.Entity<Fish>(entity =>
+            {
+                entity.ToTable("Fish", "MarineLife");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.Schooling).HasMaxLength(2);
+
+                entity.HasOne(d => d.WaterType)
+                    .WithMany(p => p.Fish)
                     .HasForeignKey(d => d.WaterTypeId)
-                    .HasConstraintName("FK_Exhibit_WaterTypeId");
-                });
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Fish_WaterTypeId");
+            });
 
-        modelBuilder.Entity<Hazard>(entity =>
-                {
-                    entity.ToTable("Hazards", "HumanEffects");
+            modelBuilder.Entity<Plant>(entity =>
+            {
+                entity.ToTable("Fish", "MarineLife");
 
-                    entity.Property(e => e.Name)
-                        .IsRequired()
-                        .HasMaxLength(100);
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
 
-                    entity.Property(e => e.Description)
-                        .IsRequired()
-                        .HasMaxLength(500);
-                });
+                entity.Property(e => e.Description).HasMaxLength(500);
 
-                modelBuilder.Entity<Trash>(entity =>
-                {
-                    entity.Property(e => e.Name)
-                             .IsRequired()
-                             .HasMaxLength(100);
+                entity.HasOne(d => d.WaterType)
+                    .WithMany(p => p.Fish)
+                    .HasForeignKey(d => d.WaterTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Plant_WaterTypeId");
+            });
 
-                    entity.Property(e => e.Description)
-                        .IsRequired()
-                        .HasMaxLength(500);
-                });
 
-                modelBuilder.Entity<Fish>(entity =>
-                {
-                    entity.ToTable("Fish", "MarineLife");
-
-                    entity.Property(e => e.Name)
-                        .IsRequired()
-                        .HasMaxLength(50);
-
-                    entity.Property(e => e.Description).HasMaxLength(500);
-
-                    entity.Property(e => e.Schooling).HasMaxLength(2);
-
-                    entity.HasOne(d => d.WaterType)
-                        .WithMany(p => p.Fish)
-                        .HasForeignKey(d => d.WaterTypeId)
-                        .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK_Fish_WaterTypeId");
-                });
-
-                modelBuilder.Entity<Plant>(entity =>
-                {
-                    entity.ToTable("Fish", "MarineLife");
-
-                    entity.Property(e => e.Name)
-                        .IsRequired()
-                        .HasMaxLength(50);
-
-                    entity.Property(e => e.Description).HasMaxLength(500);
-
-                    entity.HasOne(d => d.WaterType)
-                        .WithMany(p => p.Fish)
-                        .HasForeignKey(d => d.WaterTypeId)
-                        .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK_Plant_WaterTypeId");
-                });
-
-                
-            }
         }
     }
 }
