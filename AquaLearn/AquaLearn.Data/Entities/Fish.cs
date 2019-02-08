13bd2ce89f22bf2d5a648b508d66e3b3e61de8ce
Andy Perez <andypere01@gmail.com>
2019-02-08 16:04:37 -0500

    beginhelp [#163836139]

---
+++ AquaLearn/AquaLearn.Data/Entities/Fish.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaLearn.Data.Entities
+{
+    public class Fish
+    {
+    }
+}
---
+++ AquaLearn/AquaLearn.Data/Helpers/FishHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaLearn.Data.Helpers
+{
+    public class FishHelper
+    {
+    }
+}
