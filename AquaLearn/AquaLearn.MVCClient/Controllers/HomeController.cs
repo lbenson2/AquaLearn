256b325abc0daf6465f322585b34fbb3802b66c9
Jasper Rector <jasper.rector9@gmail.com>
2019-02-10 13:45:32 -0500

    add the login action [#163849386]

--- AquaLearn/AquaLearn.MVCClient/Controllers/HomeController.cs
+++ AquaLearn/AquaLearn.MVCClient/Controllers/HomeController.cs
@@ -25,5 +25,11 @@ namespace AquaLearn.MVCClient.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+        public IActionResult Login()
+        {
+          var user = new User();
+          return View(user);
+        }
+  }
 }
