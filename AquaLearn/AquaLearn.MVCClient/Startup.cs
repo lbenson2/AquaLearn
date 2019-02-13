7b6e921852d5e63fdc87a910da26a4b75fc4d07b
AJ <jardineang9@gmail.com>
2019-02-12 21:11:54 -0500

    Hid DbConnection String from DbContext. [163892117 finish]

--- AquaLearn/AquaLearn.MVCClient/Startup.cs
+++ AquaLearn/AquaLearn.MVCClient/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AquaLearn.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +26,10 @@ namespace AquaLearn.MVCClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AquaLearnDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("AquaLearnDb")));
+            AquaLearnDbContext.Configuration = Configuration;
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
