38ef71878d8eaf7c3cd374248933e4d52f37b0f9
Lauren <laurennbenson@hotmail.com>
2019-02-15 12:48:58 -0500

    Fixing build [164014129 finish]

--- AquaLearn/AquaLearn.MVCClient/Controllers/ExhibitController.cs
+++ AquaLearn/AquaLearn.MVCClient/Controllers/ExhibitController.cs
@@ -11,31 +11,7 @@ namespace AquaLearn.MVCClient.Controllers
     {
         public IActionResult Index()
         {
-            var i = new ExhibitViewModel();
-            i.ExhibitId = 0;
-            return View(i);
+            return View();
         }
-
-        [HttpPost]
-        public PartialViewResult AddPartialToView(string id)
-        {
-          return PartialView(id);
-        }
-
-        public IActionResult SetDeep()
-        {
-          var i = new ExhibitViewModel();
-          i.ExhibitId = 0;
-          return View(i);
-        }
-
-        public IActionResult SetCoral()
-        {
-          var i = new ExhibitViewModel();
-          i.ExhibitId = 1;
-          return View(i);
-        }
-
-    
-  }
+    }
 }
