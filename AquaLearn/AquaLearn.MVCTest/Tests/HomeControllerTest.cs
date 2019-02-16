b5d2b1eb4ce78c96bbb872cac2e883a1f22a1ce7
Jasper Rector <jasper.rector9@gmail.com>
2019-02-16 17:37:17 -0500

    add home controller tests [#164015996]

---
+++ AquaLearn/AquaLearn.MVCTest/Tests/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaLearn.MVCTest.Tests
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Test_Index()
+    {
+
+    }
+
+  }
+}
