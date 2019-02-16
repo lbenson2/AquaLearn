1b6f4a9651a0976300c57d7194b526adc20de419
Jasper Rector <jasper.rector9@gmail.com>
2019-02-16 12:57:15 -0500

    add the quiz helper test [#164015996]

---
+++ AquaLearn/AquaLearn.Tests/DataTests/QuizHelperTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AquaLearn.Data.Helpers;
+using AquaLearn.Domain.Models;
+using Xunit;
+using AquaLearn.Data;
+
+namespace AquaLearn.Tests.DataTests
+{
+  class QuizHelperTest
+  {
+  }
+}
