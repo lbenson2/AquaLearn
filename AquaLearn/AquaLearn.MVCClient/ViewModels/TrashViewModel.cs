563ad1696291b594bb50556d30f99605bb2f2547
Lauren <laurennbenson@hotmail.com>
2019-02-16 01:37:52 -0500

    add necessary view models in MVC [#163850498]

---
+++ AquaLearn/AquaLearn.MVCClient/ViewModels/TrashViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ald = AquaLearn.Domain.Models;
+using AquaLearn.Data.Helpers;
+
+namespace AquaLearn.MVCClient.ViewModels
+{
+    public class TrashViewModel
+    {
+        public List<ald.Trash> GetTrash()
+        {
+            var helper = new TrashHelper();
+            return helper.GetTrash();
+        }
+    }
+}
