151debe039513275e64d9c8dc227afd0d80fff0f
Andy Perez <andypere01@gmail.com>
2019-02-17 17:49:05 -0500

    fixit role [#164036284]

--- AquaLearn/AquaLearn.MVCTest/Tests/HomeControllerTest.cs
+++ AquaLearn/AquaLearn.MVCTest/Tests/HomeControllerTest.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using AquaLearn.MVCClient.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AquaLearn.MVCTest.Tests
