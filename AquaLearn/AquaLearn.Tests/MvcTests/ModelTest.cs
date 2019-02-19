cbe0502847552d72f95a215fc3bd5506dff4c728
Andy Perez <andypere01@gmail.com>
2019-02-18 20:13:10 -0500

    not yet again[#164065078]

--- AquaLearn/AquaLearn.Tests/MvcTests/ModelTest.cs
+++ AquaLearn/AquaLearn.Tests/MvcTests/ModelTest.cs
@@ -9,68 +9,84 @@ namespace AquaLearn.Tests.MvcTests
 {
     public class ModelTest
     {
-            UserModel sut { get; set; }
-            ExhibitModel sute { get; set; }
-            ErrorViewModel sutr { get; set; }
+        UserModel sut { get; set; }
+        ExhibitModel sute { get; set; }
+        ErrorViewModel sutr { get; set; }
+        ClassroomModel sutc { get; set; }
 
         public ModelTest()
+        {
+            sut = new UserModel()
             {
-                sut = new UserModel()
-                {
-                    //UserId = 22,
-                    //Username = "Andy",
-                    //Password = "Andy",
-                    //ClassroomId = 22
+                //UserId = 22,
+                Name = "Andy",
+                Username = "Andy",
+                Password = "Andy",
+                Quizzes = new List<Quiz>()
+                //ClassroomId = 22
 
-                };
+            };
 
             sute = new ExhibitModel()
             {
-
+                Name = "Andy",
+                Fishes = new List<Fish>(),
+                Trash = new List<Trash>(),
+                Plants = new List<Plant>()
             };
 
             sutr = new ErrorViewModel()
             {
-
+                RequestId = "Andy"
             };
 
-            }
+            sutc = new ClassroomModel()
+            {
+                Name = "Andy"
+            };
+
+        }
 
         [Fact]
         public void Test_UserModel()
         {
             Assert.IsType<int>(sut.UserId);
-            //Assert.IsType<string>(sut.Username);
-            //Assert.IsType<string>(sut.Password);
-            //Assert.IsType<string>(sut.Name);
-            //Assert.IsType<int>(sut.UserId);
+            Assert.IsType<string>(sut.Username);
+            Assert.IsType<string>(sut.Password);
+            Assert.IsType<string>(sut.Name);
+            Assert.IsType<int>(sut.RoleId);
             Assert.IsType<int>(sut.ClassroomId);
-            //Assert.IsType<List<Quiz>>(sut.Quizzes);
+            Assert.IsType<List<Quiz>>(sut.Quizzes);
         }
 
         [Fact]
         public void Test_ExhibitModel()
         {
             Assert.IsType<int>(sute.ExhibitId);
-            //Assert.IsType<string>(sut.Username);
-            //Assert.IsType<string>(sut.Password);
-            //Assert.IsType<string>(sute.Name);
-            //Assert.IsType<int>(sut.UserId);
-           // Assert.IsType<int>(sut.ClassroomId);
-            //Assert.IsType<List<Quiz>>(sut.Quizzes);
+            Assert.IsType<string>(sute.Name);
+            Assert.IsType<List<Plant>>(sute.Plants);
+            Assert.IsType<List<Trash>>(sute.Trash);
+            Assert.IsType<List<Fish>>(sute.Fishes);
+
         }
 
         [Fact]
         public void Test_ErrorViewModel()
         {
-            //Assert.IsType<string>(sutr.RequestId);
-            //Assert.IsType<string>(sut.Username);
-            //Assert.IsType<string>(sut.Password);
-            //Assert.IsType<string>(sut.Name);
-            //Assert.IsType<int>(sut.UserId);
+            Assert.IsType<string>(sutr.RequestId);
+
             Assert.IsType<bool>(sutr.ShowRequestId);
-            
+
+        }
+
+        [Fact]
+        public void Test_ClassroomModel()
+        {
+            Assert.IsType<string>(sutc.Name);
+
+            Assert.IsType<int>(sutc.ClassroomId);
+
         }
 
     }
-    }
+}
