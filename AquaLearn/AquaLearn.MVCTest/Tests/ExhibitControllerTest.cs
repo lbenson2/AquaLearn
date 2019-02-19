5108f435f83b03d16204ea8d95054495829e3403
Andy Perez <andypere01@gmail.com>
2019-02-18 21:14:20 -0500

    got rid of irrelevant code [#164065078]

--- AquaLearn/AquaLearn.MVCTest/Tests/ExhibitControllerTest.cs
+++ AquaLearn/AquaLearn.MVCTest/Tests/ExhibitControllerTest.cs
@@ -14,18 +14,18 @@ namespace AquaLearn.MVCTest.Tests
     [TestClass]
     public class ExhibitControllerTest
   {
-        [TestMethod]
-        public void Test_Index()
-        {
-            var icontroller = new ExhibitController();
-//            var iresult = icontroller.Index() as ViewResult;
-     //       var ex = (ExhibitModel) iresult.ViewData.Model;
-       //     var fishVM = (FishViewModel)iresult.ViewData.Model;
-         //   var plantVM = (PlantViewModel)iresult.ViewData.Model;
-         //   var trashVM = (TrashViewModel)iresult.ViewData.Model;
-            //Assert.IsType<int>(ex.ExhibitId);
-            // Assert.AreEqual("Index", iresult.ViewName);
-        }
+//        [TestMethod]
+//        public void Test_Index()
+//        {
+//            var icontroller = new ExhibitController();
+////            var iresult = icontroller.Index() as ViewResult;
+//     //       var ex = (ExhibitModel) iresult.ViewData.Model;
+//       //     var fishVM = (FishViewModel)iresult.ViewData.Model;
+//         //   var plantVM = (PlantViewModel)iresult.ViewData.Model;
+//         //   var trashVM = (TrashViewModel)iresult.ViewData.Model;
+//            //Assert.IsType<int>(ex.ExhibitId);
+//            // Assert.AreEqual("Index", iresult.ViewName);
+//        }
 
     }
 }
